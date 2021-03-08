ac44356b645ae3b7060ba12ee6d7a5a46e4c49dc
Shaine Hatch <shaine@squidtree.com>
2021-03-08 15:53:21 -0700

    Stop auto suspending

--- home/.config/i3/lock.sh
+++ home/.config/i3/lock.sh
@@ -6,17 +6,19 @@ killall xidlehook
 xidlehook \
   `# Don't lock when there's a fullscreen application` \
   --not-when-fullscreen \
+  `# Don't suspend when there's audio playing` \
+  --not-when-audio \
   --timer 3600 \
     'betterlockscreen --lock dimblur' \
     '' \
   &
 
-xidlehook \
-  `# Don't suspend when there's a fullscreen application` \
-  --not-when-fullscreen \
-  `# Don't suspend when there's audio playing` \
-  --not-when-audio \
-  --timer 5400 \
-    'systemctl suspend' \
-    '' \
-  &
+# xidlehook \
+  # `# Don't suspend when there's a fullscreen application` \
+  # --not-when-fullscreen \
+  # `# Don't suspend when there's audio playing` \
+  # --not-when-audio \
+  # --timer 5400 \
+    # 'systemctl suspend' \
+    # '' \
+  # &

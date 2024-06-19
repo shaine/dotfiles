722edaf7e77bb7f9948cf1f925a20e400c2a2d89
Shaine Hatch <shaine@squidtree.com>
2024-06-18 18:41:43 -0600

    Finish replacing old i3 setup

--- home/.config/i3/wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# wallpaper_path=$(ls $HOME/.config/bg/* | shuf -n 1)
-wallpaper_path=$(gsettings get org.gnome.desktop.background picture-uri | cut -c 9- | cut -d "'" -f 1)
-wal_backend=$(cat $HOME/.config/bg/.wal-backend)
-
-wal -c
-wal --backend $wal_backend -i $wallpaper_path -o "$HOME/.config/i3/wallpaper_after_hook.sh"
--- home/.config/i3/wallpaper_after_hook.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-wallpaper_path=$(cat $HOME/.cache/wal/wal)
-
-betterlockscreen -u $wallpaper_path -b 1 &
-
-$HOME/.config/lemonbar/run_lemonbar.sh &
